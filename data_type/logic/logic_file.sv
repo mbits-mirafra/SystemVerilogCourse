6cc1fb6bd3e67180362098da6408e7a0ad088b5d
mouna <mounakumarswamy@mirafra.com>
2022-08-25 02:53:42 +0530

    logic

--- data_type/logic/logic_file.sv
+++ data_type/logic/logic_file.sv
@@ -5,18 +5,18 @@
 module logic_data;
 
 //Declaring logic data type
-logic [2:0] data;
+logic [2:0]logic_data_type;
 
 initial begin
 
 // Displaying the value of logic data type(default)
-  $display("data=%b",data);
+$display("\nDefault value of logic data type =%b",logic_data_type);
 
 // Initialising the value for logic data type
-  data=3'b101;
+logic_data_type=3'b101;
 
 // Displaying the value of logic data type after initialising the value
-   $display("data=%b",data);
-
- end
- endmodule
+$display("\nValue of logic data type  after initialization =%b",logic_data_type);
+$display("");
+end
+endmodule : logic_data
