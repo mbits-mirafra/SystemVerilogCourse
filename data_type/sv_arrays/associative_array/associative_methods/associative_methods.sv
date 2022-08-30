8168b0f0cdf4cd00881718351d0dafc1935576c4
kashyapp <kashyappancholi@mirafra.com>
2022-08-30 10:28:32 +0530

    modified associative array methods file

--- data_type/sv_arrays/associative_array/associative_methods/associative_methods.sv
+++ data_type/sv_arrays/associative_array/associative_methods/associative_methods.sv
@@ -1,15 +1,17 @@
 // module declaration for associative array
-module associative_method ();
+module associative_method();
 
 // Declare an associative array abc of type int and index type string
   int abc[string];
   string variable;
+  string val1;
 
   initial begin
     $display("");
     $display("// associative array given below");
     // initialize each dynamic array with values
-    abc = '{ "vadodara" : 10 , "ahmedabad" : 25 , "surendranagar" : 38 , "rajkot" : 55};
+    abc = '{ "vadodara" : 10 , "ahmedabad" : 25 , "surendranagar" : 38 , "rajkot" : 55 ,
+    "surat":48};
 
     // printing both array values
     $display("abc = %p",abc);
@@ -41,20 +43,33 @@ module associative_method ();
     $display("");
     $display("// abc.first(index) - returns the first index value in array");
     $display("// output of abc.first(variable)");
-    abc.first(variable);
-    $display("abc[%s]=%0d",variable,abc[variable]);
+    if(abc.first(variable)) begin
+      $display("abc[%s]=%0d",variable,abc[variable]);
+    end
+
     // function last(variable): it will assign value of last index to variable
     $display("");
     $display("// abc.last(index) - returns the last index value in array");
     $display("// output of abc.last(variable)");
-    abc.last(variable);
-    $display("abc[%s]=%0d",variable,abc[variable]);
+    if(abc.last(variable)) begin
+      $display("abc[%s]=%0d",variable,abc[variable]);
+    end
 
+    // function next(index): it will give next index value which is greater than current index the 
+    // in array
+    $display("");
+    $display("// abc.next(index) - gives next index value which is greater than current index"); 
+    $display("// and it's corresponding value in array");
+    $display("// output after abc.next(val1)");
+    if(abc.next(val1)) begin
+      $display("abc[%s]=%0d",val1,abc[val1]);
+    end
+    
     // function delete(index): it will delete index from the array
     $display("");
     $display("// abc.delete(index) - deletes the index and it's corresponding value in array");
-    $display("// output after deleting rajkot index in array");
-    abc.delete("rajkot");
+    $display("// output after deleting surendranagar index in array");
+    abc.delete("surendranagar");
     $display("%p",abc);
     $display("");
   end
