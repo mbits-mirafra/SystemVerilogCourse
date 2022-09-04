11c55d10fdf93931a42b4fd8890235a923252d32
John Shahid Shaik <johnshahid@HweServer.mirafra.com>
2022-09-05 03:31:07 +0530

    fork_join_modified

--- processes/fork_join/fork_join.sv
+++ processes/fork_join/fork_join.sv
@@ -10,43 +10,45 @@
 
 module sample();
 
+  event e1;
+  int a=5,b=6,c,d;
+  string str;
   initial begin:main //This is a procedural block
 
-    #5 $display("main_Thread-1: [%0t] starting of fork-join",$time); 
+    $display("[%0t]Thread-1: Values of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d); 
+
+    //-------------------------------------------------------
+    //
+    //This is a fork-join block.
+    //In this block we can have multiple threads like
+    //begin-end,$displays
+    //Even a fork-join has nested fork-join in it.
+    //
+    //-------------------------------------------------------
 
     fork:fork_main
-      //-------------------------------------------------------
-      //
-      //This is a fork-join block.
-      //In this block we can have multiple threads like
-      //begin-end,$displays
-      //Even a fork-join has nested fork-join in it.
-      //
-      //-------------------------------------------------------
-
-      #0 $display("Thread-1: [%0t] basics of syatem verilog",$time);//Thread 1
-
-      #3 $display("Thread-2: [%0t] data types",$time);//Thread 2
       
-      #5 $display("Thread-3: [%0t] control-flow",$time);//Thread 3
-      
-      begin:first //Thread 4
-        #1 $display("Thread-4-1: [%0t] processes",$time);
-        #3 $display("Thread-4-2: [%0t] communications",$time);
+      begin:first
+        #1 a <= b;
+        b <= 7;
+        $monitor("[%0t]Thread-1: Values of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d);
+        
+        #1 ->e1;
+        c = b;
       end:first
-      
-      fork:nested_fork //Thread 5
-        #7 $display("Thread-5-1: [%0t] oops",$time);
-        #0 $display("Thread-5-2: [%0t] nested_fork",$time);
-      join:nested_fork
-      
-      #9 $display("Thread-6: [%0t]assertions",$time);//Thread 6
-      #1 $display("Thread-7: [%0t] coverages",$time);//Thread 7
+    
+      begin:second
+        wait(e1.triggered);
+        $display("[%0t] Event is triggered",$time);     
+        
+        begin:first_2 //Thread 4
+          #1 d = c;         
+        end:first_2
+
+      end:second
 
     join:fork_main
-    
-    #10 $display("main_Thread-2: [%0t] ending of fork-join",$time);
-  
+
   end:main
 
 endmodule:sample
