59529c4acbf9f6a8773115ebca678fdf7e796d12
Dampuru Vinay Kumar Reddy <vinaydampuru@HweServer.mirafra.com>
2022-09-08 02:51:23 +0530

    fork_join modified

--- processes/fork_join/fork_join.sv
+++ processes/fork_join/fork_join.sv
@@ -12,7 +12,6 @@ module sample();
 
   event e1;
   int a=5,b=6,c,d;
-  string str;
   initial begin:BEGIN_B1 //This is a procedural block
 
     $display("[%0t] Thread_T1: Values of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d); 
@@ -41,13 +40,15 @@ module sample();
         $display("[%0t] Event is triggered",$time);     
     
         begin:BEGIN_B4 //Thread 4
-          #1 d = c;         
+          #1 d = c;
+          b <= 1;
         end:BEGIN_B4
 
       end:BEGIN_B3
 
     join:FORK_F1
-
+    
+    $display("[%0t] Thread_T3: value of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d);
    end:BEGIN_B1
 
 endmodule:sample
