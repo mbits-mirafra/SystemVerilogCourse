faa4721355c4de346c29a6b141aad48bf72620ec
piyushagrawal4578 <110447788+piyushagrawal4578@users.noreply.github.com>
2022-09-10 10:57:25 +0530

    Update func_return_void.sv

--- functions/func_return_void/func_return_void.sv
+++ functions/func_return_void/func_return_void.sv
@@ -7,15 +7,14 @@ module func_return_void;
   
   initial
   begin
-    void'(display("\t ----output for function void return type-----"));
-    void'(display("\t passing string to function for displaying"));
+    (display("\t ----output for function void return type-----");
+    (display("\t passing string to function for displaying");
   end
   //-------------------------------------------------------
   // Writing the definition of the function.
   //-------------------------------------------------------
-  function int display(string str);
+  function void display(string str);
     $display("%s",str);
-      return 0;
   endfunction : display
 
 endmodule : func_return_void
