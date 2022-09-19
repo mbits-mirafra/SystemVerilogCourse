84a46901adc53d76889ef73c9c757e1e01f2abf7
gopals <gopalsuthar@mirafra.com>
2022-09-20 02:56:30 +0530

    adding clocking block folder

---
+++ interface/Clocking_Block/clocking_block_case2/test.sv
@@ -0,0 +1,24 @@
+//module: test
+module test(dff.tb intf);
+
+//task:drv
+task drv;
+
+  //loop
+  repeat(10)
+begin
+  //test triggering at posedge
+  @(posedge intf.clk )
+
+  //randomzing the d
+  intf.d <= $random;
+  $display("test side [%0t] : d_tb_drive = %d q_dut_sample = %d",$time,intf.d, intf.q);
+end
+$finish;
+endtask 
+
+//calling the task drv
+  initial begin
+     drv();
+  end 
+endmodule :test
