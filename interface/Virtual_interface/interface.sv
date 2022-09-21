5c1e3cbc7708a994b956f9f26b324f0e2537c467
Dilip K <110412474+DilipKrishnappa@users.noreply.github.com>
2022-09-21 20:32:21 +0530

    Delete interface directory

--- interface/Virtual_interface/interface.sv
+++
@@ -1,8 +0,0 @@
-interface adder();
-  //-------------------------------------------------------
-  // decalring the signals
-  //-------------------------------------------------------
-  logic in_a,in_b,in_c;
-    logic out_sum,out_carry;
-
-  endinterface
