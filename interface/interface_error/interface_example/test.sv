18110f35505dad0abd9b4021fbf40c49fa649993
mouna <mounakumarswamy@mirafra.com>
2022-09-29 00:37:51 +0530

    interface added

--- interface/interface_error/interface_example/test.sv
+++ interface/interface_error/interface_example/test.sv
@@ -4,7 +4,7 @@ module tb(and_if inf);
 initial begin
   $display("\n// and gate output");
   $monitor("\ninput_a=%b\t input_b=%b\t output_y=%b",inf.input_a,inf.input_b,inf.output_y);
-  inf.y=0;
+  inf.output_y=0;
   inf.input_a = 0; inf.input_b = 0; 
   #1;
   inf.input_a = 1; inf.input_b = 0; 
