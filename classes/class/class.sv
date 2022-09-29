b5d184ee8429bbe2a5d7c0d10b0cb3131df63754
Vinaydampuru <110411714+Vinaydampuru@users.noreply.github.com>
2022-09-29 15:28:04 +0530

    properties and methods of a class lables

--- classes/class/class.sv
+++ classes/class/class.sv
@@ -2,7 +2,7 @@
 //class is user define datatype that include the propeties
 //and method
 //-------------------------------------------------------
-class home;   //define class  
+class home; //define class  
 
  //declare class properties  
   bit light;  
@@ -13,16 +13,16 @@ class home;   //define class
  task open_electricity();  
    switch = "ON";
    $display("switch is %0s so electricity is open",switch);  
- endtask  
-endclass  
+ endtask:open_electricity  
+endclass:home
 
 module check_electricity;  
- initial begin  
+ initial begin:BEGIN_I  
   home h1;   //creating handle  
   h1=new();   //Creating Object for the Handle  
   h1.light=1;   // access the properties
   h1.fan=1;
   h1.open_electricity;  //access the method
   $display("light is %0d(ON) and fan is also %0d(ON)",h1.light,h1.fan);
-end  
-endmodule 
+end:BEGIN_I
+endmodule:check_electricity
