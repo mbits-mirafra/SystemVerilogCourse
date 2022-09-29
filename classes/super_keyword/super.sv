16580842538d39527677da15ffcbfc6ef97dc333
Vinaydampuru <110411714+Vinaydampuru@users.noreply.github.com>
2022-09-29 15:45:29 +0530

    super keyword in lable

--- classes/super_keyword/super.sv
+++ classes/super_keyword/super.sv
@@ -9,23 +9,23 @@ class base_class;
   function void display();    //method of class
     switch="ON";
     $write("switch is %s " ,switch);
-  endfunction
-endclass
+  endfunction:display
+endclass:base_class
 
 class sub_class extends base_class;
  string fan="ON";
   function void display();
     super.display; //define the super keyword inside the sub_class
     $write("that's why fan is %s \n" ,fan);
-  endfunction 
-endclass
+  endfunction:display
+endclass:sub_class
 
 sub_class s1;  //creating handle for class
 
 module super_example;
-initial begin
+initial begin:BEGIN_I
   s1 =new();    //create an object
   s1.display();  //access the sub_class method
-end
-endmodule
+end:BEGIN_I
+endmodule:super_example
 
