27cbe28c87c3403ff0f4da470f13c607df359f6b
Vinaydampuru <110411714+Vinaydampuru@users.noreply.github.com>
2022-09-29 15:21:53 +0530

    lables for class object

--- classes/class_object/object.sv
+++ classes/class_object/object.sv
@@ -2,7 +2,7 @@
 //object is created by using the new() function
 //
 //..............................................................
-class home;    //define class
+class home; //define class
 
   //declare class properties
   bit light;
@@ -13,19 +13,22 @@ class home;    //define class
   task open_electricity();
     switch = "ON";
     $display("switch is %s so electricity is open",switch);
-  endtask
-endclass
+  endtask:open_electricity
+
+endclass:home
 
 module tb;
-home h1;     //creating handle
-initial begin
-      
-    //call the new() function of class and create an object
-      h1=new();
-      if (h1==null)
-      $display("object is empty");
-      else
-        $display("object is not empty");
-   end
- endmodule
 
+  home h1;     //creating handle
+
+  initial begin:BEGIN_I
+  
+    //call the new() function of class and create an object
+    h1=new();
+    if (h1==null)
+      $display("object is empty");
+    else
+      $display("object is not empty");
+  end:BEGIN_I
+  
+endmodule:tb
