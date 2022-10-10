9c6f974ac8034e67249a39db73ba296dd94ad7d2
kashyapp <kashyappancholi@mirafra.com>
2022-10-11 02:10:36 +0530

    impliation operator

---
+++ assertion/implication_operator/nonoverlapped_implication/assertions.sv
@@ -0,0 +1,34 @@
+module nonoverlapped_assertion;
+  bit clk,a,b,valid;
+  
+  always #5 clk = ~clk; //clock generation
+  
+  //generating 'a'
+  initial begin
+    valid=1;
+        a=1; b=1;
+    #15 a=1; b=0;
+    #10 b=1;
+    #12 b=0;
+    #10 a=0; b=1;
+    valid=0;
+    #15 a=1; b=0;
+    #100 $finish;
+  end
+  
+  property p;
+    @(posedge clk) valid |=> (a ##3 b);
+  endproperty
+  
+  //calling assert property
+  a_1: assert property(p)
+    $info("pass");
+    else
+      $info("fail");
+
+      initial begin
+        $dumpfile("waveform.vcd");
+        $dumpvars();
+      end
+    
+endmodule
