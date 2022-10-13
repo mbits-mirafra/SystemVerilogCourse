6ab3c55d812569823564923a80861c3bab317fe8
Bhavanasandrala <110509375+Bhavanasandrala@users.noreply.github.com>
2022-10-13 16:56:04 +0530

    Update file_handles.sv

--- Misc_constructs/file_handling/file_handles/file_handles.sv
+++ Misc_constructs/file_handling/file_handles/file_handles.sv
@@ -1,10 +1,16 @@
 module file_handles;
 int f;
-initial begin
-f=$fopen("file_handle","w");
-$fdisplay(f,"fileoparations");
-$fdisplay(f,"sv course");
-$fclose(f);
+
+ initial begin
+ 
+  f=$fopen("file_handle","w");
+  
+   $fdisplay(f,"fileoperations");
+   $fdisplay(f,"sv course");
+  
+ $fclose(f);
+
 end 
+
 endmodule
 
