141f3ad77d03b76826dc7a12a2dbbe4ad52e408d
gopals <gopalsuthar@mirafra.com>
2022-11-16 10:38:38 +0530

    changing in formal argument

--- assertion/formal_argument/formal_argument.sv
+++ assertion/formal_argument/formal_argument.sv
@@ -1,64 +1,52 @@
-
 module formal_argument;
-logic clk = 0;
-logic req,gnt;
-logic c,d;
+bit clk,a,b,c,d;
 
-sequence notype_seq (X, Y);
-  (~X & Y)  ##1  (~X & ~Y);
+always #5 clk = ~clk;
+
+sequence notype_seq (X,Y);
+  X && Y;
 endsequence
 
-sequence withtype_seq (logic X, logic Y);
-  (~X & Y)  ##1  (~X & ~Y);
+sequence withtype_seq (bit X, bit Y);
+  X && Y;
 endsequence
 
-property req_gnt_notype_prop(P,Q);
+property a_b_notype_prop(a,b);
   @ (posedge clk)
-  req |=> notype_seq(P,Q);
+  a |-> notype_seq(a,b);
 endproperty
 
-property c_d_type_prop(logic P, logic Q);
+property c_d_type_prop(bit c, bit d);
   @ (posedge clk)
-  c |=> withtype_seq(P,Q);
+  c |-> withtype_seq(c,d);
 endproperty
 
-req_gnt_notype_assert : assert property (req_gnt_notype_prop(req,gnt));
-c_d_type_assert       : assert property (c_d_type_prop(c,d));
+a_b_notype_assert : assert property (a_b_notype_prop(a,b))
+$info("assertion passed");
+else
+  $error("assertion failed");
+  c_d_type_assert   : assert property (c_d_type_prop(c,d))
+  $info("assertion passed");
+  else
+    $error("assertion failed");
 
-always @ (posedge clk)
-  gnt <= req;
-
-  always @ (posedge clk)
-    d  <= c;
-
-    always  #3  clk = ~clk;
     initial begin
-      // Make the assertion pass
-      #10  @ (posedge clk) req  <= 1;
-      @ (posedge clk) req <= 0;
-      // Make the assertion fail
-      #10  @ (posedge clk) req  <= 1;
-      repeat (2) @ (posedge clk);
-      req <= 0;
-
-
-
-
-      // Make the assertion pass
-      #10  @ (posedge clk) c  <= 1;
-      @ (posedge clk) c <= 0;
-      // Make the assertion fail
-      #10  @ (posedge clk) c  <= 1;
-      repeat (2) @ (posedge clk);
-      c <= 0;
-      #50  $finish;
+      a=0; b=0;
+      c=0; d=0;
+      #15 a=1; b=0;
+      c=0; d=1;
+      #10 a=1; b=1;
+      c=1; d=1;
+      #10 a=1; b=0;
+      c=1; d=1;
+      #10 a=1; b=1;
+      c=1; d=0;
+      #20;
+      $finish;
     end
 
-    initial begin                                                                                       
-      $dumpfile                                                                                         
-      ("waveform.vcd");                                                                                 
-      $dumpvars();                                                                                      
-    end                                                                                                 
-    endmodule                                                                                           
-
-
+    initial begin
+      $dumpfile("waveform.vcd");
+      $dumpvars();
+    end
+    endmodule
