74a3525d54eb5ce28382533eb3102092359d8f63
Muneeb <muneebullashariff@gmail.com>
2022-12-07 22:50:46 +0530

    Adding the comments in Makefile for waveform dump for synopsys simualtor

--- for_loop/for_loop.sv
+++ for_loop/for_loop.sv
@@ -6,27 +6,13 @@
 // sddfkshfkkhdsjkhdkfskfhdjkhs
 //--------------------------------------------------------------------------------------------
 module tb;
-  bit clk;
 
   string array [2] = '{"apple", "orange"};
 
   initial begin
-    clk = 0;
     for (int i=0; i < $size(array); i++) begin
       $display("array[%0d] = %s", i, array[i]);
     end
   end
 
-  always begin
-    #5 clk = ~clk;
-  end
-
-  initial begin
-    $dumpfile("verilog.vcd");
-    $dumpvars();
-  end
-
-  initial begin
-    #50 $finish;
-  end
 endmodule: tb
