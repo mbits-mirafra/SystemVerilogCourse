1625d2a30252d9f49caf9900fe9beebd1ebddc4a
gopals <gopalsuthar@mirafra.com>
2025-02-25 11:44:28 +0530

    updated and added tasks in task directory

--- tasks/simple_task/simple_task.sv
+++ tasks/simple_task/simple_task.sv
@@ -1,16 +1,17 @@
-
 module simple_task();
-  
-  int multiplicand=5,multiplicator=6,result;
-  initial
-  begin
+  int multiplicand=5;
+  int multiplicator=6;
+  int result;
+
+  initial begin
     $display("\t ----output of simple task calling----");
-  mul(multiplicand,multiplicator,result);
-  $display("\t @ %0t ns , %0d X %0d = %0d",$time,multiplicand,multiplicator,result);
+    mul(multiplicand,multiplicator,result);
+    $display("\t @ %0t ns , %0d X %0d = %0d",$time,multiplicand,multiplicator,result);
   end
 
   task mul(input int var1,var2,output int res);
-    #2 res=var1*var2;
+    #2;
+    res=var1*var2;
   endtask
 
 endmodule
