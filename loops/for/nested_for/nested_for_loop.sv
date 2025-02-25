8f114db56eacdedca2c39a80ea00fbbaaa8fc2de
gopals <gopalsuthar@mirafra.com>
2025-02-25 16:59:32 +0530

    updated and added loops in loops directory

--- loops/for/nested_for/nested_for_loop.sv
+++ loops/for/nested_for/nested_for_loop.sv
@@ -1,15 +1,10 @@
-module table_for_loop;
-
-  initial 
-  begin
-
+module nested_for_loop;
+  initial begin
     $display("\t ----Tables using nested for---");
 
-    for (int i=1;i<=2;i++)
-    begin
+    for (int i=1;i<=2;i++) begin
       $display("\n\t%0d Table:\n",i);
-      for(int j=1,k=0;j<=10;j++)
-      begin
+      for(int j=1,k=0;j<=10;j++) begin
         k=i*j;
         $display("\t %0d X %0d = %0d",i,j,k);
       end
@@ -17,4 +12,4 @@ module table_for_loop;
     $display("\n\t ----out of loop----");
   end
 
-endmodule:table_for_loop
+endmodule:nested_for_loop
